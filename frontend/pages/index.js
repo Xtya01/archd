c06d17a1d2b564783cc4f57cbc2fd804adeb69d6
Xtya01 <151998390+Xtya01@users.noreply.github.com>
2026-04-23 08:40:14 +0400

    Add Home component for Archive File Manager

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Archive File Manager</h1>
+      <p>Personal Archive.org cloud drive</p>
+
+      <a href="/upload">📤 Upload</a><br /><br />
+      <a href="/my-archive">📁 My Archive</a>
+    </div>
+  );
+}
